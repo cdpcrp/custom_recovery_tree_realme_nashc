5fe425ec5c8bc55072f8de109148824525cb502e
C:\Users\Carlo Dee <109793453+cd-Crypton@users.noreply.github.com>
2022-12-08 00:11:54 +0800

    Reconfigure, make it much cleaner.

--- twrp_nashc.mk
+++ twrp_nashc.mk
@@ -1,29 +1,30 @@
-#
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# Device Information
+PRODUCT_DEVICE := nashc
+PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme 8 4G
+PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
+PRODUCT_RELEASE_NAME := RMX3085
 
-# Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+# Inherit from Prebuilt Products.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from GSI product
+# Inherit from GSI Products.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit from RMX3085L1 device
-$(call inherit-product, device/realme/nashc/device.mk)
+# Inherit from RMX3085 Device Tree.
+$(call inherit-product, device/$(PRODUCT_BRAND)/$(PRODUCT_DEVICE)/device.mk)
 
-# Inherit some common twrp stuff.
+# Inherit from PBRP-common stuff, if building PBRP.
 $(call inherit-product-if-exists, vendor/pb/config/common.mk)
+
+# Inherit from TWRP-common stuff, if building TWRP.
 $(call inherit-product-if-exists, vendor/twrp/config/common.mk)
 
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := nashc
-PRODUCT_NAME := twrp_nashc
-PRODUCT_BRAND := Realme
-PRODUCT_MODEL := Realme 8 RMX3085
-PRODUCT_MANUFACTURER := Realme
+PRODUCT_GMS_CLIENTID_BASE := android-$(PRODUCT_MANUFCATURER)
+
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+# SPDX-License-Identifier: Apache-2.0
